a63da9c43d00abe0a22f4c312905e8f56b76a107
Sévan Bendit <bendit.sevan@gmail.com>
2022-03-07 09:27:17 +0100

    Ouvrir la caméra

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <h1>Scan QR code</h1>
+  <button>Scanner</button>
+  <div class="card" style="width: 18rem;">
+    <qrcode-stream @decode="onDecode" @init="onInit" />
+    <div class="card-body">
+      <p class="card-text">Please scan the qrcode event registration that we have sent via your email.</p>
+      <hr>
+      <div class="alert" role="alert">
+
+      </div>
+    </div>
+  </div>
+
+  <h1>Scan RFID</h1>
+
 </template>
 
 <script>
+import { QrcodeStream } from 'vue3-qrcode-reader'
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: { QrcodeStream },
+  async onDecode (result) {
+
+  },
+  async onInit (promise) {
+
+  }
 })
 </script>
