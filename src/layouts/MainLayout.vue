f9b809efe9bbaed37c37fba4df819ed6a9a72568
Sévan Bendit <bendit.sevan@gmail.com>
2022-03-09 10:19:54 +0100

    Enlèvement du menu de base de quasar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,27 @@
 <template>
+
   <q-layout view="lHh Lpr lFf">
+
+    <!--
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+    <q-toolbar>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleLeftDrawer"
+      />
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+      <q-toolbar-title>
+        Quasar App
+      </q-toolbar-title>
+
+      <div>Quasar v{{ $q.version }}</div>
+    </q-toolbar>
+
 
     <q-drawer
       v-model="leftDrawerOpen"
@@ -39,10 +43,13 @@
       </q-list>
     </q-drawer>
 
+  -->
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
+
 </template>
 
 <script>
