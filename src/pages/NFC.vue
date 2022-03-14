89eebfc96b66f381a1abb4a9d77e87bb024b3c06
Sévan Bendit <bendit.sevan@gmail.com>
2022-03-14 13:28:32 +0100

    Scan de carte NFC et renvoie d'une valeur OK

--- src/pages/NFC.vue
+++ src/pages/NFC.vue
@@ -1,22 +1,111 @@
-
 <template>
-
+  <div text-center>
+      <span v-if="compatible">
+        Waiting
+        <br>
+        <q-btn @click="dialog = true">History</q-btn>
+        <q-dialog v-model="dialog" scrollable>
+          <q-card>
+            <div>History</div>
+            <ul style="height: 300px;">
+                <li v-for="item in items" v-bind:key="item">
+                  <div>
+                    <p v-text="item"></p>
+                  </div>
+                </li>
+                <li v-if="items.length === 0">
+                  <div>
+                    <p class="text-center">Pas d'historique</p>
+                  </div>
+                </li>
+            </ul>
+          </q-card>
+        </q-dialog>
+      </span>
+    <q-btn v-on:click="showSettings" v-else-if="nfc_disabled">Settings</q-btn>
+    <span v-else>Pas disponible</span>
+  </div>
 </template>
 
 <script>
 
-
 export default {
   name: 'NFC',
-  data() {
+  data(){
     return {
-
+      compatible: true,
+      nfc_disabled: false,
+      dialog: false,
+      items: JSON.parse((localStorage.getItem("scanHistory")||"[]")),
     }
   },
-  methods: {}
+  watch:{
+    items: function (v) {
+      // Watch push on the items data. If a new item is push save it to the « localStorage ».
+      localStorage.setItem("scanHistory", JSON.stringify(this.items));
+    }
+  },
+  mounted(){
+    // When the view is mounted, register the scan tag event.
+    this.registerTagEvent();
+  },
+  beforeDestroy(){
+    // When the view is destroyed (user leave), unregister the scan tag event, to avoid scanning tag in other view
+    this.unregisterTagEvent();
+  },
+  methods: {
+    registerTagEvent(){
+      // Unregister previously « resume » event listener.
+      document.removeEventListener("resume", this.registerTagEvent, false);
+
+      if (typeof(nfc) !== "undefined"){
+        // Nfc is available, waiting for scan
+        nfc.addTagDiscoveredListener(this.displayTagId, this.success, this.error);
+      }else{
+        // Plugin not present or failed to initialized.
+        this.error();
+      }
+    },
+    unregisterTagEvent(){
+      // Test if the plugin is defined
+      if (typeof(nfc) !== "undefined") {
+        nfc.removeTagDiscoveredListener(this.displayTagId);
+      }
+    },
+    displayTagId(nfcEvent){
+      // Decode tag data from the plugin
+      let tag = nfcEvent.tag;
+      let tagId = nfc.bytesToHexString(tag.id);
+
+      // Push the new tag to the saved list
+      this.items.push(tagId);
+
+      // Show the tag Id to the user
+      console.log(tagId)
+    },
+    error(e){
+      // Manage the state
+      if(e === "NFC_DISABLED"){
+        this.compatible = false;
+        this.nfc_disabled = true;
+      }else{
+        this.nfc_disabled = false;
+        this.compatible = false;
+      }
+    },
+    success(){
+      this.compatible = true;
+      this.nfc_disabled = false;
+      console.log("Nfc initialized");
+    },
+    showSettings(){
+      // Trigger the phone settings to enable the Nfc settings
+      nfc.showSettings();
+
+      // To refresh the state of the nfc, we add a listener to the « resume » event.
+      // The resume event is triggered by cordova when the app is « Resumed ».
+      document.addEventListener("resume", this.registerTagEvent, false);
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/pages/QRcode.vue
+++ src/pages/QRcode.vue
@@ -6,7 +6,7 @@
     <label for="checkbox">Est-ce un retour ? (Si oui, cochez) </label>
     <input type="checkbox" id="checkbox" v-model="checked">
     <!-- Afficher avec une variable si la case est cochée (true) ou pas (false) -->
-    <label for="checkbox">{{ retouroupas() }}</label>
+    <label for="checkbox">{{ retourouemprunt() }}</label>
 
   </div>
 </template>
@@ -15,19 +15,20 @@
 export default {
   data() {
     return {
+      test: "",
       checked: false,
-      retour: "",
+      retouroupas: "emprunt",
       imageSrc: '',
       title: '', //resultat du QR code scanné
       description: ''
     }
   },
   methods: {
-    retouroupas(){
+    retourouemprunt(){
       if(this.checked===true){
-        return this.retour="retour";
+        return this.retouroupas="retour";
       }else{
-        return this.retour="pasretour";
+        return this.retouroupas="emprunt";
       }
     },
     scanImage() {
@@ -41,7 +42,10 @@ export default {
             result.format +
             '\n' +
             'Cancelled: ' +
-            result.cancelled
+            result.cancelled +
+            '\n' +
+            'retour ou emprunt: ' +
+            this.retour
           )
         },
         error => {
@@ -53,7 +57,7 @@ export default {
           showTorchButton: true, // iOS and Android
           torchOn: false, // Android, launch with the torch switched on (if available)
           saveHistory: true, // Android, save scan history (default false)
-          prompt: 'Place a QR code inside the scan area', // Android
+          prompt: 'Placez le QR code à l\'intérieur de la zone', // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
           orientation: 'portrait', // Android only (portrait|landscape), default unset so it rotates with the device
