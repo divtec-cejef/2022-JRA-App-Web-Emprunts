3557c1a33c97658e43230e06db750f8007338543
Sévan Bendit <bendit.sevan@gmail.com>
2022-03-14 09:10:43 +0100

    Scan dans le sens portrait du téléphone

--- src/pages/QRcode.vue
+++ src/pages/QRcode.vue
@@ -40,10 +40,10 @@ export default {
           showTorchButton: true, // iOS and Android
           torchOn: false, // Android, launch with the torch switched on (if available)
           saveHistory: true, // Android, save scan history (default false)
-          prompt: 'Place a barcode inside the scan area', // Android
+          prompt: 'Place a QR code inside the scan area', // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          orientation: 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+          orientation: 'portrait', // Android only (portrait|landscape), default unset so it rotates with the device
           disableAnimations: true, // iOS
           disableSuccessBeep: true // iOS and Android
         }
