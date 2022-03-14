16559d4e802549107780ed1a395cb5ae50149736
Sévan Bendit <bendit.sevan@gmail.com>
2022-03-14 09:09:52 +0100

    Flash pas activé par défaut mais bouton pour l'activer

--- src/pages/QRcode.vue
+++ src/pages/QRcode.vue
@@ -38,7 +38,7 @@ export default {
           preferFrontCamera: false, // iOS and Android
           showFlipCameraButton: true, // iOS and Android
           showTorchButton: true, // iOS and Android
-          torchOn: true, // Android, launch with the torch switched on (if available)
+          torchOn: false, // Android, launch with the torch switched on (if available)
           saveHistory: true, // Android, save scan history (default false)
           prompt: 'Place a barcode inside the scan area', // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
